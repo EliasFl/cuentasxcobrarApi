3607769aff3698ab038819e81066b20476ef014f
elias <eliasflores13@outlook.com>
2020-03-11 02:44:52 -0700

    Added a method to update the tipoDocumento resource

--- src/main/kotlin/com/unapec/cuentasxcobrar/controller/TiposDocumentosController.kt
+++ src/main/kotlin/com/unapec/cuentasxcobrar/controller/TiposDocumentosController.kt
@@ -3,6 +3,8 @@ package com.unapec.cuentasxcobrar.controller
 import com.unapec.cuentasxcobrar.model.TiposDocumentos
 import com.unapec.cuentasxcobrar.repository.TiposDocumentosRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -21,4 +23,22 @@ class TiposDocumentosController(
     @PostMapping("/tiposDocumentos")
     fun createTipoDocumento(@Valid @RequestBody tiposDocumento: TiposDocumentos) =
             repository.save(tiposDocumento)
+
+    @PutMapping("/tiposDocumentos/{id}")
+    @Throws(ResourceNotFoundException::class)
+    fun updateTipoDocumento(
+            @PathVariable id: Long,
+            @Valid @RequestBody tipoDocumentoDetail: TiposDocumentos
+    ): ResponseEntity<TiposDocumentos> {
+        val tipoDocumento: TiposDocumentos = repository.findById(id).
+                orElseThrow {
+                    ResourceNotFoundException("Tipo de documento no encontrado")
+                }
+        tipoDocumento.descripcion = tipoDocumentoDetail.descripcion
+        tipoDocumento.cuentaContable = tipoDocumentoDetail.cuentaContable
+        tipoDocumento.estado = tipoDocumentoDetail.estado
+
+        val updatedTipoDocumento = repository.save(tipoDocumento)
+        return ResponseEntity.ok(updatedTipoDocumento)
+    }
 }
