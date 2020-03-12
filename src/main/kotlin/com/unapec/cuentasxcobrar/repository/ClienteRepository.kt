63b2ad8744c90e7f93e7a438c3a852c6f02e8967
elias <eliasflores13@outlook.com>
2020-03-11 23:57:27 -0700

    Added the necesary code to the clienteRepository

--- src/main/kotlin/com/unapec/cuentasxcobrar/repository/ClienteRepository.kt
+++ src/main/kotlin/com/unapec/cuentasxcobrar/repository/ClienteRepository.kt
@@ -1,4 +1,9 @@
 package com.unapec.cuentasxcobrar.repository
 
-interface ClienteRepository {
+import com.unapec.cuentasxcobrar.model.Cliente
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ClienteRepository: CrudRepository<Cliente, Long> {
 }
